ec34b387af74c834b362582e039bd337898b65a3
Kaung122333 <akuma122333@gmail.com>
2024-03-04 16:18:56 +0630

    Admin Side

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,68 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import Button from "@/src/components/Button";
+import { useCart } from "@/src/providers/CartProvider";
+import { PizzaSize } from "@/src/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
+  const router = useRouter();
+  const [selectedSize, setSelectSize] = useState<PizzaSize>("M");
+  const product = products.find((p) => p.id.toString() === id);
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+  if (!product) {
+    return <Text>Pizza not found!</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaImage }}
+      />
+
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  textContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default ProductDetailsScreen;
